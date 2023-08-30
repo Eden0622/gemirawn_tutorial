f908afd94a836afbe608e471d398d7ee0b4b7070
Eden <noah@noteasy.kr>
2023-08-30 13:21:55 +0900

    - 공지사항 짝수 인덱스에 이미지+버튼 기능구현 - 파일명 수정 commit

---
+++ lib/board/board_detail.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gemirawn_tutorial/board/board.dart';
+import 'package:gemirawn_tutorial/data/models/notice.dart';
+import 'package:intl/intl.dart';
+
+import '../config/constants.dart';
+import '../data/models/notice.dart';
+
+class BoardView extends StatelessWidget {
+  const BoardView({super.key, required this.notice});
+
+  final Notice notice;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: SvgPicture.asset('assets/icon/appbar_prev_icon.svg'),
+            color: blackColor,
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const Board()));
+            },
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      notice.store,
+                      style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: color1),
+                    ),
+                    Text(DateFormat('yyyy.MM.dd').format(notice.date), style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: color1))
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  notice.notice_title,
+                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w700, color: blackColor),
+                ),
+                const SizedBox(height: 16),
+                Text(notice.notice_content),
+                if (notice.imageUrl != null && noticeList.indexOf(notice) % 2 == 0)
+                  Container(
+                      margin: const EdgeInsets.symmetric(vertical: 24),
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Image.network(notice.imageUrl!),
+                          ),
+                          const SizedBox(height: 24),
+                          const Divider(
+                            thickness: 1,
+                            height: 1,
+                          ),
+                          const SizedBox(height: 16),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.white,
+                                elevation: 0,
+                                side: const BorderSide(color: primaryColor),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8)
+                              ),
+                              minimumSize: const Size(double.infinity, 48)
+                            ),
+                            child: const Text(
+                              '공지사항 관련 자료.JPG',
+                              style: TextStyle(color: primaryColor, fontSize: 14, fontWeight: FontWeight.w500),
+                            ),
+                          )
+                        ],
+                      ))
+              ],
+            ),
+          ),
+        ));
+  }
+}
--- lib/board/board_list.dart
+++ lib/board/board_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gemirawn_tutorial/board/board_view.dart';
+import 'package:gemirawn_tutorial/board/board_detail.dart';
 import 'package:gemirawn_tutorial/config/constants.dart';
 import 'package:intl/intl.dart';
 
@@ -15,10 +15,17 @@ class BoardList extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: GestureDetector(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => const BoardView()));
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => BoardView(
+                        notice: notice,
+                      )));
         },
         child: Container(
+          height: 156,
+          margin: const EdgeInsets.all(16.0),
           decoration: ShapeDecoration(
             color: Colors.white,
             shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
@@ -34,50 +41,47 @@ class BoardList extends StatelessWidget {
           child: Column(
             children: [
               Container(
-                padding: const EdgeInsets.symmetric(horizontal: 120, vertical: 15),
-                decoration: const BoxDecoration(
-                  color: primaryColor,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(8),
-                    topRight: Radius.circular(8)
-                  )
+                width: double.infinity,
+                height: 48,
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(color: primaryColor, borderRadius: BorderRadius.only(topLeft: Radius.circular(8), topRight: Radius.circular(8))),
+                child: Text(
+                  notice.notice_title,
+                  style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: Colors.white),
                 ),
-                child: Text('${notice.notice_title}',
-                  style: const TextStyle( fontSize: 14, fontWeight: FontWeight.w500, color: Colors.white),),
                 //decoration: ,
               ),
-              Container(
-                padding: const EdgeInsets.all(16),
-                child: Text('${notice.notice_content}',
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(16),
+                  child: Text(
+                    notice.notice_content,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500, height: 1.6),
+                  ),
+                ),
               ),
-              const Divider(height: 2, indent: 16, endIndent: 16, color: Color(0xFFefefef),thickness: 1,),
+              const Divider(
+                height: 1,
+                indent: 16,
+                endIndent: 16,
+                color: Color(0xFFefefef),
+                thickness: 1,
+              ),
               Container(
                 padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(8),
-                        bottomRight: Radius.circular(8)
-                    )
-                ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('${notice.store}',
-                      style: const TextStyle(
-                      fontSize: 12, fontWeight: FontWeight.w500, color: color1
-                    ),),
                     Text(
-                        DateFormat('yyyy.MM.dd').format(notice.date),
-                      style: const TextStyle(
-                          fontSize: 12, fontWeight: FontWeight.w500, color: color1)
-                    )
+                      notice.store,
+                      style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: color1),
+                    ),
+                    Text(DateFormat('yyyy.MM.dd').format(notice.date), style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: color1))
                   ],
                 ),
               ),
-              //SizedBox(height: 16,),
             ],
           ),
         ),
@@ -85,3 +89,4 @@ class BoardList extends StatelessWidget {
     );
   }
 }
+
